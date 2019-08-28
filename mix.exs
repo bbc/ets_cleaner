722b62a1be6aa2bde3c576489d6049fdcf3a2191
James Bowers <james@bowers.dev>
2019-08-28 12:05:09 +0100

    lower elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EtsCleaner.MixProject do
     [
       app: :ets_cleaner,
       version: "0.1.0",
-      elixir: "~> 1.9",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
